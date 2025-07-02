ee8f8a621e2ea42012289fe9520d69414af430d8
Ben Barlow <ben.j.barlow.1@gmail.com>
2025-07-02 09:38:21 +0100

    chore: remove commented code

--- shiny_for_python/01_app_dynamic_plots_global_input.py
+++ shiny_for_python/01_app_dynamic_plots_global_input.py
@@ -5,20 +5,10 @@ from shiny import ui, render, App, module, reactive
 MAX_PLOTS = 5
 
 
-# UI module for a single plot
 @module.ui
 def plot_ui():
-    # Returns a placeholder for a plot that will be rendered server-side
     return ui.output_plot("plot")
 
-
-# @module.ui
-# def plot_slider():
-#     # Returns a placeholder for a slider that will be rendered server-side
-#     return ui.output_ui("slider")
-
-
-# Server module to generate and render each plot
 @module.server
 def plot_server(
     input, output, session, plot_id: int, mult: int, max_plots: int = MAX_PLOTS
@@ -35,13 +25,6 @@ def plot_server(
         ax.set_ylim(0, max_plots)
         ax.set_title(f"1: {plot_id}. mult is {mult}. n is {max_plots}")
         return fig
-
-
-# @module.server
-# def slider_server(input, output, session, slider_id: int):
-#     @render.ui
-#     def slider():
-#         return ui.input_slider
 
 app_ui = ui.page_fluid(
     ui.panel_title("Dynamic number of plots"),
@@ -78,23 +61,15 @@ def server(input, output, session):
                 disabled=True,
             )
 
-    # Function to dynamically render UI components (plots)
     @render.ui
     def plots():
-        # Ensure value is an integer and available
-        # n_plots = int(req(input.n_plots()))
         n_plots = current_n_plots.get()
         slider_val = input.global_slider()
-        # Dynamically create server-side functions for each plot
         for i in range(1, n_plots + 1):
             plot_server(f"plot_{i}", plot_id=i, mult=slider_val, max_plots=MAX_PLOTS)
-        # Dynamically create UI components for each plot
-        # Note that the names must match the server-side functions
-        # But they're close to each other in code
         to_return = []
         for i in range(1, n_plots + 1):
             to_return.append(plot_ui(f"plot_{i}"))
-            # to_return.append(plot_slider(f"slider_{i}"))
         return (to_return,)
 
 
