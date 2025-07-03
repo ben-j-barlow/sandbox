00e6216132cc69c54762803a55b26e69767bd88e
Ben Barlow <ben-j-barlow.1@gmail.com>
2025-07-03 12:29:49 +0100

    feat: add injection of user inputs to compile sql

--- shiny_for_python/app.py
+++ shiny_for_python/app.py
@@ -1,16 +1,75 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from shiny import ui, render, App, module, reactive
+from shiny import ui, render, App, module, reactive, req
 from shinywidgets import render_plotly, output_widget, render_widget
 from datetime import datetime
 import pandas as pd
 import plotly.express as px
 import re
 from dataclasses import dataclass
-from abc import ABC
+from abc import ABC, abstractmethod
 from numbers import Real
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 PLOTS = 1
+INITIAL_QUERY = "SELECT * FROM stocks WHERE price > 5 AND my_var = {my_var, type=slider, min=1, max=10, step=1} AND {row_condition, type=text_box}"
+
+@dataclass
+class ParamConfig(ABC):
+    """Base parameter configuration using a dataclass."""
+    param_id: str
+    sql_identifier: str
+
+    @property
+    @abstractmethod
+    def param_type(self) -> str:
+        """Abstract property to be implemented by subclasses."""
+        pass
+
+@dataclass
+class SliderParamConfig(ParamConfig):
+    """Slider configuration inheriting from the base dataclass."""
+
+    min_val: float
+    max_val: float
+    step: float
+    # Use a field with a default_factory to handle the mutable default logic
+    default: float
+    param_type: str = "slider"
+    
+    def __post_init__(self):
+        """Called after the dataclass's own __init__."""
+        self.min_val = float(self.min_val)
+        self.max_val = float(self.max_val)
+        self.step = float(self.step)
+
+        if self.default is None:
+            self.default = self.min_val
+
+@dataclass
+class TextBoxParamConfig(ParamConfig):
+    """Text box configuration inheriting from the base dataclass."""
+    default: str
+    param_type: str = "text_box"
+
+    def __post_init__(self):
+        """Called after the dataclass's own __init__."""
+        if self.default is None:
+            self.default = "xyz = 2"
+        self.default = str(self.default)
+
+
+def compile_query(input, parameter_universe: list[ParamConfig], plot_id: int, parameterised_query: str):
+    # build {param_identifier: value} dict
+    for_replace = {ele.sql_identifier: str(getattr(input, get_plot_input_id(plot_id, ele.param_id, ele.param_type))()) for ele in parameter_universe}
+    
+    # use {param_identifier: value} dict to compile the query
+    pattern = re.compile("|".join(map(re.escape, for_replace.keys())))
+    compiled_query = pattern.sub(lambda match: for_replace[match.group(0)], parameterised_query)
+    return compiled_query
 
 def parse_parameterised_sql(sql) -> set[ParamConfig]:
     matches = set(re.findall(r'\{(.*?)\}', sql))
@@ -25,7 +84,7 @@ def parse_parameterised_sql(sql) -> set[ParamConfig]:
                 try:
                     param_config = SliderParamConfig(
                         param_id=param_id,
-                        sql_identifier=match,
+                        sql_identifier=append_parenthesis(match),
                         min_val=param_properties['min'],
                         max_val=param_properties['max'],
                         step=param_properties['step'],
@@ -37,7 +96,7 @@ def parse_parameterised_sql(sql) -> set[ParamConfig]:
                 try:
                     param_config = TextBoxParamConfig(
                         param_id=param_id,
-                        sql_identifier=match,
+                        sql_identifier=append_parenthesis(match),
                         default=param_properties.get('default', None),
                     )
                 except KeyError as e:
@@ -61,44 +120,9 @@ df = pd.DataFrame(
     }
 )
 
-
-
-@dataclass
-class ParamConfig(ABC):
-    """Base parameter configuration using a dataclass."""
-    param_id: str
-    sql_identifier: str
-
-
-@dataclass
-class SliderParamConfig(ParamConfig):
-    """Slider configuration inheriting from the base dataclass."""
-
-    min_val: float
-    max_val: float
-    step: float
-    # Use a field with a default_factory to handle the mutable default logic
-    default: float
-    
-    def __post_init__(self):
-        """Called after the dataclass's own __init__."""
-        self.min_val = float(self.min_val)
-        self.max_val = float(self.max_val)
-        self.step = float(self.step)
-
-        if self.default is None:
-            self.default = self.min_val
-
-@dataclass
-class TextBoxParamConfig(ParamConfig):
-    """Text box configuration inheriting from the base dataclass."""
-    default: str
-
-    def __post_init__(self):
-        """Called after the dataclass's own __init__."""
-        if self.default is None:
-            self.default = ""
-        self.default = str(self.default)
+def append_parenthesis(match: str) -> str:
+    "Append {} parentheses at start and finish."    
+    return "{" + match + "}"
 
 def get_plot_input_id(plot_id: int, param_id: str, param_type: str) -> str:
     """Generate a unique input ID for a plot parameter."""
@@ -110,17 +134,7 @@ def get_plot_input_div_id(plot_id: int, param_id: str, param_type: str) -> str:
 
 def get_selector_for_plot_input_removal(plot_id: int) -> str:
     """Generate a selector for removing all input elements for a specific plot."""
-    #$('div[id^="div_input_plot_1_"]')
     return f'div[id^="div_input_plot_{plot_id}"]'
-
-def get_param_type_from_class_name(class_name: str) -> str:
-    """Get the parameter type from the class name."""
-    if class_name == "SliderParamConfig":
-        return "slider"
-    elif class_name == "TextBoxParamConfig":
-        return "text_box"
-    else:
-        raise ValueError(f"Unsupported parameter class name: {class_name}")
     
 def make_items():
     return [
@@ -129,7 +143,10 @@ def make_items():
             ui.layout_columns(
                 ui.card(
                     ui.card_header(f"Plot {i}"),
-                    output_widget(f"plot_{i}"),
+                    ui.TagList(
+                        output_widget(f"plot_{i}_plot"),
+                        ui.output_text(f"plot_{i}_query"),
+                    ),
                     full_screen=True,
                 ),
                 ui.card(
@@ -149,7 +166,7 @@ app_ui = ui.page_fluid(
     ui.card(
         ui.card_header("Enter parameterised SQL"),
         ui.layout_columns(
-            ui.input_text_area("sql_input", label="SQL Input", value="SELECT * FROM stocks WHERE price > 5 AND {ticker, type=slider, min=1, max=10, step=1} AND {my_param, type=text_box}", rows=6, width="100%"),
+            ui.input_text_area("sql_input", label="SQL Input", value=INITIAL_QUERY, rows=6, width="100%"),
             ui.output_text_verbatim("output_sql_parameters", ),
             width="100%",
             col_widths=[6, 6],
@@ -160,24 +177,47 @@ app_ui = ui.page_fluid(
     ui.accordion(*make_items(), id="acc", multiple=True),
 )
 
+
 def server(input, output, session):
+    plot_id = 1
     parameter_universe = reactive.Value(list())
-    
+    parameterised_sql = INITIAL_QUERY
+
+    @reactive.effect
+    @reactive.event(input.run_query_1)
+    def _():
+        parameter_universe_ = parameter_universe.get()
+        compile_query(
+            input,
+            parameter_universe=parameter_universe_,
+            plot_id=plot_id,
+            parameterised_query=parameterised_sql,
+        )
+
     @render_widget
-    def plot_1():
+    @reactive.event(input.run_query_1)
+    def plot_1_plot():
+        slider_val = getattr(input, get_plot_input_id(plot_id=1, param_id="my_var", param_type="slider"))()
         return px.scatter(
             df,
             x="agg_metric_period",
             y="agg_metric_value",
             color="is_spike",
-            title=f"Plot 1: Slider value is {input.slider_1()}",
+            title=f"Plot 1: Slider value is {slider_val}",
         )
     
-    @reactive.effect
+    @render.text
     @reactive.event(input.run_query_1)
-    def _handle_run_query_1():
+    def plot_1_query():
         parameter_universe_ = parameter_universe.get()
-        
+        return compile_query(
+            input,
+            parameter_universe=parameter_universe_,
+            plot_id=plot_id,
+            parameterised_query=parameterised_sql,
+        )
+
+
     @reactive.effect
     @reactive.event(input.remove_ui)
     def _handle_remove_ui():
@@ -191,35 +231,29 @@ def server(input, output, session):
         parameter_universe_ = parse_parameterised_sql(parameterised_sql)
         parameter_universe.set(parameter_universe_)
 
-        
         # Add new UI components based on the parsed parameters
-        
-        cnt = 0
         for param_config in parameter_universe_:
-            cnt += 1
             param_id = param_config.param_id
 
             if isinstance(param_config, SliderParamConfig):
-                param_type = "slider"
                 to_add = ui.input_slider(
-                    get_plot_input_id(1, param_id, param_type),
+                    get_plot_input_id(plot_id, param_id, param_config.param_type),
                     label=param_id,
                     value=param_config.default,
                     min=param_config.min_val,
                     max=param_config.max_val,
                     step=param_config.step,
                 )
-            elif isinstance(param_config, TextBoxParamConfig):                
-                param_type = "text_box"
+            elif isinstance(param_config, TextBoxParamConfig):
                 to_add = ui.input_text(
-                    get_plot_input_id(1, param_id, param_type),
+                    get_plot_input_id(plot_id, param_id, param_config.param_type),
                     label=param_id,
                     value=param_config.default,
                 )
                 
             ui.insert_ui(
-                ui=ui.div(to_add, id=get_plot_input_div_id(plot_id=1, param_id=param_id, param_type=param_type)),
-                selector="#input_card_1",
+                ui=ui.div(to_add, id=get_plot_input_div_id(plot_id=plot_id, param_id=param_id, param_type=param_config.param_type)),
+                selector=f"#input_card_{plot_id}",
                 session=session,
             )
 
